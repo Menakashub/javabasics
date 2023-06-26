82138c449918b5d5c50ac65d3322c0fdcfa78322
Menakashub <menakagy@gmail.com>
2023-06-26 10:47:59 -0400

    reverse string added

---
+++ JavaPrograms/src/ReverseEachWordInString.java
@@ -0,0 +1,18 @@
+import java.util.Iterator;
+
+public class ReverseEachWordInString {
+
+	public static void main(String[] args) {
+		String s = "Welcome to java";
+		String word[]=s.split(" ");
+		String reversestring="";
+		for(String w:word) {
+			String reverseword="";
+			for(int i=w.length()-1;i>=0;i--) {
+				reverseword=reverseword+w.charAt(i);
+			}
+			reversestring=reversestring+reverseword+" ";
+		}
+		System.out.println(reversestring);
+	}
+	}
